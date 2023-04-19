644e876ecb87265a06e2e4c967506d1d420e7c84
chandrabhansolanki <cbss1994@gmail.com>
2023-04-19 20:22:45 +0530

    changes in file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from "./logo.svg";
 // import "./App.css";
-import Header from "./Component /Header/Header";
-import Slider from "./Component /Slider/Slider";
-import AstroCircle from "./Component /astrocircle/astroCircle";
+import Header from "./Component/Header/Header";
+import Slider from "./Component/Slider/Slider";
+import AstroCircle from "./Component/astrocircle/astroCircle";
 
 function App() {
   return (
     <div className="">
       <Header />
-     <Slider />
-     <AstroCircle />
+      <Slider />
+      <AstroCircle />
     </div>
   );
 }
