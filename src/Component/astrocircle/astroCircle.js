43ae1f3f7a35894c74f645aa63c0b020ef059fb8
chandrabhansolanki <cbss1994@gmail.com>
2023-04-19 22:12:46 +0530

    changes

--- src/Component/astrocircle/astroCircle.js
+++ src/Component/astrocircle/astroCircle.js
@@ -1,34 +1,48 @@
 import React from "react";
 import { chakra } from "../../images/images";
 import "./astroCircle.css";
-import { avatar } from "../../images/images";
+import {
+  avatar,
+  loveProblem,
+  marriage,
+  job,
+  lucky,
+  education,
+} from "../../images/images";
 
 const astroCircle = () => {
   return (
-    <div style={{display: "flex", justifyContent: "center" , alignItems:'center', flexDirection:"column" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <div style={{ display: "flex", justifyContent: "center" }}>
         <p style={{ fontSize: "24px", color: "red" }}>
           * Many problem One Solution *
         </p>
       </div>
 
-      <div  className='mainCardContainer'>
+      <div className="mainCardContainer">
         <div style={{ display: "flex", flexDirection: "column" }}>
           <div className="cardcontainer">
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
-            <p>Money problem</p>
+            <img src={education} style={{ height: "57px", width: "55px" }} />
+            <p>Education Problems </p>
           </div>
           <div className="cardcontainer">
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
-            <p>Money problem</p>
+            <img src={loveProblem} style={{ height: "57px", width: "55px" }} />
+            <p>Love Related Problem </p>
           </div>
           <div className="cardcontainer">
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
-            <p>Money problem</p>
+            <img src={marriage} style={{ height: "57px", width: "55px" }} />
+            <p>Marriage Problem </p>
           </div>
           <div className="cardcontainer">
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
-            <p>Money problem</p>
+            <img src={job} style={{ height: "57px", width: "55px" }} />
+            <p>Job/Business Problems </p>
           </div>
         </div>
         <div className="chakracontainer">
@@ -36,20 +50,20 @@ const astroCircle = () => {
         </div>
         <div style={{ display: "flex", flexDirection: "column" }}>
           <div className="cardcontainertwo">
-            <p>Money problem</p>
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
+            <p>Health Problems</p>
+            <img src={lucky} style={{ height: "57.16px", width: "55.66px", textAlign:"left" }} />
           </div>
           <div className="cardcontainertwo">
-            <p>Money problem</p>
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
+            <p>Lucky Number / Color</p>
+            <img src={lucky} style={{ height: "57.16px", width: "55.66px" }} />
           </div>
           <div className="cardcontainertwo">
-            <p>Money problem</p>
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
+            <p>Child Birth Issues</p>
+            <img src={lucky} style={{ height: "57.16px", width: "55.66px" }} />
           </div>
           <div className="cardcontainertwo">
-            <p>Money problem</p>
-            <img src={avatar} style={{ height: "40px", width: "40px" }} />
+            <p>Husband Wife Problems</p>
+            <img src={lucky} style={{ height: "57.16px", width: "55.66px" }} />
           </div>
         </div>
       </div>
